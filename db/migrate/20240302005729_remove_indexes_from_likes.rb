9291f63be0e9b468f141808adc17aa867c499fca
Njeri Kamau <64022619+njerimk@users.noreply.github.com>
2024-03-02 10:39:16 -0500

    Delete db directory

--- db/migrate/20240302005729_remove_indexes_from_likes.rb
+++
@@ -1,4 +0,0 @@
-class RemoveIndexesFromLikes < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/migrate/20240302012749_drop_account.rb
+++
@@ -1,6 +0,0 @@
-class DropAccount < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :accounts
-
-  end
-end
