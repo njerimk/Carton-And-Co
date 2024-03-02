e27c16f3c084ca148380e967acf78c6734dbc362
njerimk <nmurugikamau@gmail.com>
2024-03-02 11:44:31 -0500

    updated react version and redo create-react-app

--- server/app/serializers/comic_page_serializer.rb
+++ server/app/serializers/comic_page_serializer.rb
@@ -1,3 +1,26 @@
 class ComicPageSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :page_name, :page_desc, :comic_id, :image_url
+
+  def initialize(comic_page_object)
+    @comic_page = comic_page_object
+  end
+
+  def to_serialized_json
+        @comic_page.to_json(:include => {
+            :comic => {:except => [:comic_desc, :updated_at]},
+            :comments => {:except => [:updated_at] 
+              }}
+            )
+  end 
+
+  def comic 
+      Comic.with_attachedimage.where(comic_id: object.id)
+  end
+  
+  def image_url
+      variant = object.image.variant(resize: "960x1440")
+      return rails_representation_url(variant, only_path: true)
+  end 
+
 end
+
--- server/app/serializers/comic_serializer.rb
+++ server/app/serializers/comic_serializer.rb
@@ -1,5 +1,29 @@
 # app/serializers/comic_serializer.rb
 class ComicSerializer < ActiveModel::Serializer
   attributes :id, :title, :comic_desc, :image_url, :bg_image_url
+
+  def initialize(comic_object)
+    @comic = comic_object
+  end
+
+  def to_serialized_json
+    @comic.to_json(:include => {
+    :likes => {:only => [:id, :num_likes]},
+    :users=>{:only => [:id, :user_name, :image_url]},
+    :comic_pages =>{:except => [:page_desc, :updated_at]}
+    })
+  end
+
+  def user
+    User.with_attached_image.where(user_id: object.id)
+  end
+
+
+
+  def image_url
+    variant = object.image.variant(resize: "110x110")
+    return rails_representation_url(variant, only_path: true)
+  end 
+
 end
 
